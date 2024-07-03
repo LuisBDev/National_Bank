252a438c678a5d83fe6994243e3e6022dc7aac08
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2024-07-03 14:32:08 -0500

    allow cors service payment

--- src/main/java/com/nationalbank/nationalbankperu/controller/ServicePaymentController.java
+++ src/main/java/com/nationalbank/nationalbankperu/controller/ServicePaymentController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/service-payment")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ServicePaymentController {
 
     @Autowired
